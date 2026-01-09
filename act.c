0651f210c2a0367e9741a49062a69c19ac24b9ad
Gucchi3 <nnykb-k@outlook.jp>
2026-01-10 00:12:41 +0900

    SiLU関数の指数関数呼び出しを修正し、精度を向上させるためにexpをexpfに変更

--- act.c
+++ act.c
@@ -21,7 +21,7 @@ Tensor* RELU6(Tensor* input_tensor){
 
 Tensor* SiLU(Tensor* input_tensor){
     for(int i=0; i < input_tensor->H * input_tensor->W * input_tensor->C; i++){
-    input_tensor->data[i] =  input_tensor->data[i] / (1 + exp(- input_tensor->data[i])); 
+    input_tensor->data[i] =  input_tensor->data[i] / (1 + expf(- input_tensor->data[i])); 
   }
   return input_tensor;
 }
