d4ad294d023ba9224ac44027fb11f883e532bf90
gucchi3 <nnykb-k@outlook.jp>
2026-01-10 18:28:15 +0900

    Linear層のヘッダーファイルと実装ファイルを新規作成

---
+++ include/Linear.h
@@ -0,0 +1,17 @@
+#include "Tensor.h"
+
+/*-----------------------------------------
+Linear：ごく一般的な線形層を提供します。
+must：input_tensor, weight_tensor, bias_tensor
+
+explanation：
+　ごく一般的な線形層です。　
+　出力形状はweight_tensorから自動的に計算されます。
+
+//!! 注意 !!//
+　//! 入力データ配置順(HWCかCHW)が、重みの配置順と一致しているかを確認してください。
+　//! 本リポジトリでは、基本的にすべてHWCで統一してあります。
+　//! pytorchでは基本CHWであることに注意してください。
+　//! pytorch -> weight.hに展開時には、permute(CHW_to_HWC)関数を提供する予定です。
+-------------------------------------------*/
+Tensor* Linear(Tensor* input_tensor, W_Tensor* weight_tensor, B_Tensor* bias_tensor);
---
+++ src/Linear.c
@@ -0,0 +1,10 @@
+#include "Tensor.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+
+
+Tensor* Linear(Tensor* input_tensor, W_Tensor* weight_tensor, B_Tensor* bias_tensor){
+// now making...
+}
