93ffc260314f6c1cd6d7c82786660e797c08108e
gucchi3 <nnykb-k@outlook.jp>
2026-01-10 21:53:22 +0900

    検証用重み

--- weight.c
+++ weight.c
@@ -36,3 +36,19 @@ B_Tensor conv_bias = {
     .OC = 3,
     .data = conv1_bias_data
 };
+
+/*----------------------------------------------
+Linear Debug Tensors
+------------------------------------------------*/
+W_Tensor linear_weight = {
+  .OC = 2,
+  .INC = 3,   // 入力のチャンネル数
+  .H = 8,     // 入力の高さ
+  .W = 8,     // 入力の幅
+  .data = linear_debug_weight_data
+};
+
+B_Tensor linear_bias = {
+  .OC = 2,
+  .data = linear_debug_bias_data
+};
